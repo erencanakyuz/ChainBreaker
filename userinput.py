d1e63ecfd7aca04dcd85d161e72dc9847ce4ff67
erencanakyuz <erencakyuz@gmail.com>
2025-06-26 20:17:15 +0300

    A1

--- userinput.py
+++ userinput.py
@@ -1,10 +1,1 @@
-#user_input = input("prompt: ")
-
-user_input = input("prompt: ")
-print(f"User entered: {user_input}")
-
-if user_input.lower() == "stop":
-    print("Exiting...")
-    exit()
-else:
-    print("Continue with next task based on input...")
+ 
